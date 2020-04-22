62ae76d8b1c0d17f4baf97d3017f6e55f8a6ad67
adilene dueñas pérez <duenasperezadilene.com>
2020-04-22 17:35:39 -0500

    Initial commit

---
+++ app/src/main/java/duenas/perez/recyclerview/MiTarjeta.kt
@@ -0,0 +1,12 @@
+package duenas.perez.recyclerview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MiTarjeta : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mi_tarjeta)
+    }
+}
